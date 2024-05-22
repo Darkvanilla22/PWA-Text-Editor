7cbd45417508f5a64d9b1c89ef1c733beef01812
Finian Corbett <finian97@gmail.com>
2024-05-22 19:43:18 -0400

    Added logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  // Open the database
+  const db = await openDB('jate', 1);
+
+  // Create a transaction and access the object store
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Add the content to the object store
+  const request = store.put({ content });
+
+  // Get confirmation of the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
