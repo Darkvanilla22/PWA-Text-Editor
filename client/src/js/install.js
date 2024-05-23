a9c3b38da9d30eb71a4414ae0a629d3ac622b9f1
Finian Corbett <finian97@gmail.com>
2024-05-22 22:56:17 -0400

    Reverted changes to install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,35 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the mini-infobar from appearing on mobile
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the hidden class from the install button
-  butInstall.classList.toggle('hidden', false);
+    // Prevent Chrome 76 and later from showing the mini-infobar
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = event;
+    // Update UI notify the user they can add to home screen
+    butInstall.style.display = 'block';
 });
 
+// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
-  }
-  // Show the install prompt
-  promptEvent.prompt();
-  // Wait for the user to respond to the prompt
-  const { outcome } = await promptEvent.userChoice;
-  // Optionally, send analytics event with outcome of user choice
-  console.log(`User response to the install prompt: ${outcome}`);
-  // We've used the prompt, and can't use it again, throw it away
-  window.deferredPrompt = null;
-  // Hide the install button
-  butInstall.classList.toggle('hidden', true);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We no longer need the prompt. Clear it up.
+    deferredPrompt = null;
+    // Hide the install button
+    butInstall.style.display = 'none';
 });
 
+// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  // Log install to analytics
-  console.log('👍', 'appinstalled', event);
-  // Clear the deferredPrompt so it can be garbage collected
-  window.deferredPrompt = null;
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    // Log the installation to your analytics provider
+    console.log('PWA was installed', event);
+    // Optionally, send analytics event to indicate successful install
+    console.log('PWA was installed');
 });
