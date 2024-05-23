0b6535a78732414e1e57b48d3324f9920c45b627
Finian Corbett <finian97@gmail.com>
2024-05-22 21:31:43 -0400

    Fixed paths in about server.js and htmlRoutes.js files

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +14,7 @@ require('./routes/htmlRoutes')(app);
 
 // Serve the index.html file for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
