65d52032dbf43e658c595ffe3c170b7e17b479c8
Finian Corbett <finian97@gmail.com>
2024-05-22 21:45:46 -0400

    Reverted changes to paths for index.html

--- server/server.js
+++ server/server.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, '../../dist')));
-
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Import routes
 require('./routes/htmlRoutes')(app);
-
-// Serve the index.html file for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
-});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
