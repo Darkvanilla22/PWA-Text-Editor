785b75150baa115675da3741a3e96c7c30173862
Finian Corbett <finian97@gmail.com>
2024-05-22 22:32:58 -0400

    Rebuilt database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,40 +15,26 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-
-  // Open the database
-  const db = await openDB('jate', 1);
-
-  // Create a transaction and access the object store
-  const tx = db.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-
-  // Add the content to the object store
-  const request = store.put({ content });
-
-  // Get confirmation of the request
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET all from the database');
-
-  // Open the database
-  const db = await openDB('jate', 1);
-
-  // Create a transaction and access the object store
-  const tx = db.transaction('jate', 'readonly');
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-
-  // Get all data from the object store
-  const request = store.getAll();
-
-  // Get confirmation of the request
+  const request = store.get(1);
   const result = await request;
-  console.log('Data retrieved from the database', result);
-  return result;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
 };
 
 initdb();
