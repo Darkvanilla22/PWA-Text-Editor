5e92590b3e5583e64c7487f78cc46daed5763e10
Finian Corbett <finian97@gmail.com>
2024-05-25 08:57:03 -0400

    Added several changes to all aspects of the code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initdb = async () => {
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -11,44 +11,29 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.log('PUT to the database');
-
-  // Open the database
-  const db = await openDB('jate', 1);
-
-  // Create a transaction and access the object store
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-
-  // Add the content to the object store
-  const request = store.put({ content });
-
-  // Get confirmation of the request
-  const result = await request;
-  console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('GET all from the database');
-
-  // Open the database
-  const db = await openDB('jate', 1);
-
-  // Create a transaction and access the object store
-  const tx = db.transaction('jate', 'readonly');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-
-  // Get all data from the object store
-  const request = store.getAll();
-
-  // Get confirmation of the request
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data retrieved from the database', result);
-  return result;
+  console.log('🚀 - data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,89 +3,71 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = (env, argv) => {
-  // Determine if the current build mode is production
   const isProduction = argv.mode === 'production';
 
   return {
-    // Set the mode based on the build environment
     mode: isProduction ? 'production' : 'development',
-
-    // Define the entry points for the application
     entry: {
-      main: path.resolve(__dirname, 'src/js/index.js'), // Main entry point
-      install: path.resolve(__dirname, 'src/js/install.js') // Install entry point
+      main: path.resolve(__dirname, 'src/js/index.js'),
+      install: path.resolve(__dirname, 'src/js/install.js')
     },
-
-    // Specify the output configuration
     output: {
-      filename: '[name].bundle.js', // Use the entry name for the output file
-      path: path.resolve(__dirname, 'dist'), // Output directory
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-
-    // Define the plugins used in the build process
     plugins: [
-      // Plugin to generate the HTML file and inject bundles
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'), // Path to the HTML template
-        title: 'J.A.T.E' // Title of the HTML document
+        template: path.resolve(__dirname, 'index.html'),
+        title: 'J.A.T.E'
       }),
-      // Plugin to inject the service worker (only in production)
       isProduction && new InjectManifest({
-        swSrc: path.resolve(__dirname, 'src-sw.js'), // Source service worker file
-        swDest: 'service-worker.js', // Output service worker file
+        swSrc: path.resolve(__dirname, 'src-sw.js'),
+        swDest: 'service-worker.js',
       }),
-      // Plugin to generate the PWA manifest
       new WebpackPwaManifest({
-        fingerprints: false, // Do not add fingerprints to the filenames
-        inject: true, // Inject the manifest into the HTML
-        name: 'Just Another Text Editor', // Full name of the app
-        short_name: 'JATE', // Short name of the app
-        description: 'A simple text editor', // Description of the app
-        background_color: '#272822', // Background color
-        theme_color: '#31a9e1', // Theme color
-        start_url: '/', // Start URL
-        publicPath: '/', // Public path
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#272822',
+        theme_color: '#31a9e1',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve(__dirname, 'src/images/logo.png'), // Path to the icon image
-            sizes: [96, 128, 192, 256, 384, 512], // Different icon sizes
-            destination: path.join('assets', 'icons'), // Destination directory for icons
+            src: path.resolve(__dirname, 'src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
-    ].filter(Boolean), // Filter out any false values (such as the conditional InjectManifest plugin)
-
-    // Define the module rules for handling different file types
+    ].filter(Boolean),
     module: {
       rules: [
         {
-          test: /\.css$/i, // Match CSS files
-          use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.m?js$/, // Match JavaScript files
-          exclude: /(node_modules|bower_components)/, // Exclude node_modules and bower_components directories
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
           use: {
-            loader: 'babel-loader', // Use babel-loader for transpiling JavaScript files
+            loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'], // Use the preset-env preset
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'], // Use additional Babel plugins
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
             },
           },
         },
       ],
     },
-
-    // Configure the development server
     devServer: {
-      contentBase: path.resolve(__dirname, 'dist'), // Serve content from the dist directory
-      compress: true, // Enable gzip compression
-      port: 8080, // Port number for the dev server
-      open: true, // Open the browser after server starts
+      contentBase: path.resolve(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      open: true,
     },
   };
 };
